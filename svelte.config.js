b89ffb6be06537fa507a4f06c3ca13958e9dc29c
Th1nkK1D <witheep@gmail.com>
2021-12-07 13:50:16 +0200

    Add WindiCSS

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import WindiCSS from 'vite-plugin-windicss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,10 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			plugins: [WindiCSS()]
+		}
 	}
 };
 
