a44275f9216cfcd9f7bf72e383ce85adbbe8dbc5
Th1nkK1D <witheep@gmail.com>
2021-12-07 18:09:26 +0200

    Add footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,10 @@
 	import Projects from '../components/projects/projects.svelte';
 	import Hero from '../components/hero/hero.svelte';
 	import Survey from '../components/survey/survey.svelte';
+	import Footer from '../components/footer.svelte';
 </script>
 
 <Hero />
 <Projects />
 <Survey />
+<Footer />
