2ddc9ad16af93697225043e6f0e1618e6b38a9b1
Th1nkK1D <witheep@gmail.com>
2021-12-07 18:40:35 +0200

    Add projects anchor link

--- src/components/projects/projects.svelte
+++ src/components/projects/projects.svelte
@@ -2,7 +2,7 @@
 	import ProjectCard from './project-card.svelte';
 </script>
 
-<div class="bg-black px-3 py-36 md:py-48 text-white">
+<div id="projects" class="bg-black px-3 py-36 md:py-48 text-white">
 	<div class="max-w-screen-lg mx-auto space-y-12">
 		<ProjectCard
 			title="Know Your Rights หลักประกันสิทธิพลเมืองและสิทธิทางการเมืองที่ควรรู้"
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+	import smoothscroll from 'smoothscroll-polyfill';
+	import { onMount } from 'svelte';
 	import 'virtual:windi.css';
 	import '../styles/typography.css';
+
+	onMount(() => smoothscroll.polyfill());
 </script>
 
 <slot />
 
 <style>
+	:global(html) {
+		scroll-behavior: smooth;
+	}
 	:global(body) {
 		@apply typo-p1;
 	}
