ea15bb9eae5da53b698734089889c3fe8a588e0d
Th1nkK1D <witheep@gmail.com>
2021-12-07 18:15:46 +0200

    Add metadata

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,20 @@
 	import Hero from '../components/hero/hero.svelte';
 	import Survey from '../components/survey/survey.svelte';
 	import Footer from '../components/footer.svelte';
+
+	const META_TITLE = 'WeVis';
+	const META_DESCRIPTION = 'We visualise data for democracy';
+	const META_IMAGE = 'https://wevis.info/images/og.png';
 </script>
+
+<svelte:head>
+	<title>{META_TITLE}</title>
+	<meta name="description" content={META_DESCRIPTION} />
+	<meta property="og:title" content={META_TITLE} />
+	<meta property="og:description" content={META_DESCRIPTION} />
+	<meta property="og:image" content={META_IMAGE} />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
 
 <Hero />
 <Projects />
