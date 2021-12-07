214e64b4e148c2f7866d33d3227cf6f272f7fa18
Th1nkK1D <witheep@gmail.com>
2021-12-07 17:59:44 +0200

    Add survey section

---
+++ src/components/survey/survey.svelte
@@ -0,0 +1,33 @@
+<script>
+	import ExternalLink from '../external-link.svelte';
+	import Lottie from '../lottie.svelte';
+</script>
+
+<div class="relative">
+	<Lottie config={{ path: 'lotties/survey.json' }} />
+	<div
+		class="absolute inset-0 flex flex-col justify-center items-center space-y-2 md:space-y-4 px-3"
+	>
+		<h2 class="typo-h2 md:typo-h1 font-black text-white text-center">
+			ประชาธิปไตยของคุณหน้าตาแบบไหน?
+		</h2>
+		<ExternalLink
+			href="#"
+			class="flex flex-row items-center space-x-3 bg-mint px-6 py-2 filter hover:brightness-120"
+		>
+			<span>ร่วมแชร์</span>
+			<svg
+				width="16"
+				height="15"
+				viewBox="0 0 16 15"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M15.7071 8.20711C16.0976 7.81658 16.0976 7.18342 15.7071 6.79289L9.34315 0.428932C8.95262 0.0384078 8.31946 0.0384078 7.92893 0.428932C7.53841 0.819457 7.53841 1.45262 7.92893 1.84315L13.5858 7.5L7.92893 13.1569C7.53841 13.5474 7.53841 14.1805 7.92893 14.5711C8.31946 14.9616 8.95262 14.9616 9.34315 14.5711L15.7071 8.20711ZM0 8.5L15 8.5V6.5L0 6.5L0 8.5Z"
+					fill="black"
+				/>
+			</svg>
+		</ExternalLink>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Projects from '../components/projects/projects.svelte';
 	import Hero from '../components/hero/hero.svelte';
+	import Survey from '../components/survey/survey.svelte';
 </script>
 
 <Hero />
 <Projects />
+<Survey />
