4ac101655508e6a04c0092325316e21bc44f566e
Th1nkK1D <witheep@gmail.com>
2021-12-08 11:40:39 +0200

    Update project card and projects details

--- src/components/projects/project-card.svelte
+++ src/components/projects/project-card.svelte
@@ -8,23 +8,27 @@
 	export let href: string = '';
 </script>
 
-<ExternalLink
-	{href}
-	class="relative flex flex-col lg:(flex-row space-x-12) -lg:space-y-6 items-center"
->
+<div class="relative">
 	{#if !href}
 		<div class="absolute inset-0 flex justify-center items-center bg-black bg-opacity-60">
 			<p class="typo-h1">COMING SOON...</p>
 		</div>
 	{/if}
-	<div class="lg:w-lg"><img src={image} alt={title} /></div>
-	<div class="flex-1 space-y-4 lg:space-y-8 max-w-lg">
-		<p class="typo-p2">{date}</p>
-		<h2 class="typo-h2">{title}</h2>
-		<div class="flex flex-row">
-			{#each tags as tag}
-				<p class="typo-p2 px-2 py-1 mr-2 mb-2 bg-ultramarine">{tag}</p>
-			{/each}
+	<ExternalLink
+		{href}
+		class="flex flex-col lg:(flex-row space-x-12) -lg:space-y-6 items-center {href
+			? 'transform hover:scale-102 transition-transform duration-300 ease-in-out'
+			: ''}"
+	>
+		<div class="lg:w-lg"><img src={image} alt={title} /></div>
+		<div class="flex-1 space-y-4 lg:space-y-8 max-w-lg">
+			<p class="typo-p2">{date}</p>
+			<h2 class="typo-h2">{title}</h2>
+			<div class="flex flex-row">
+				{#each tags as tag}
+					<p class="typo-p2 px-2 py-1 mr-2 mb-2 bg-ultramarine">{tag}</p>
+				{/each}
+			</div>
 		</div>
-	</div>
-</ExternalLink>
+	</ExternalLink>
+</div>
--- src/components/projects/projects.svelte
+++ src/components/projects/projects.svelte
@@ -5,14 +5,21 @@
 <div id="projects" class="bg-black px-3 py-36 md:py-48 text-white">
 	<div class="max-w-screen-lg mx-auto space-y-12">
 		<ProjectCard
+			title="CONstitution LAB ห้องออกแบบรัฐธรรมนูญ"
+			date="Oct 04, 2021"
+			image="https://conlab.conlabth.com/og-images/default.png"
+			tags={['#Quiz', '#Exploratory']}
+			href="https://conlab.conlabth.com/"
+		/>
+		<ProjectCard
 			title="Know Your Rights หลักประกันสิทธิพลเมืองและสิทธิทางการเมืองที่ควรรู้"
-			date="Mar 07, 2021"
+			date="Jan 2022"
 			image="images/projects/know-your-rights.png"
 			tags={['#Data Viz/Stories']}
 		/>
 		<ProjectCard
 			title="Life Hack ชี้เป้า!! รวมช่องทางใช้งาน e-service ของรัฐบาล ที่ในชีวิตนี้คุณหนีไม่พ้นแน่นอน"
-			date="Mar 07, 2021"
+			date="Jan 2022"
 			image="images/projects/e-services.png"
 			tags={['#Data Viz/Stories']}
 		/>
